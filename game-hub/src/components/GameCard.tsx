7d0de672d08f05a8a8794dd16c1fa0ddd6a896d9
robberbadeia <robber.badeia@gmail.com>
2024-06-29 15:15:28 +0300

    Commit

--- game-hub/src/components/GameCard.tsx
+++ game-hub/src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
+import Emoji from "./Emoji";
 
 interface Props {
   game: Game;
